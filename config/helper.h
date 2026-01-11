6f23cecf46334c5c705af9872df77fd574a84d08
inc0nnu <ivo@tulkens.com>
2026-01-11 22:08:14 +0100

    defined layer names and aesthetics in helper.h

--- config/helper.h
+++ config/helper.h
@@ -75,7 +75,7 @@
 #define ZMK_COMBO_4_ARGS(name, combo_bindings, keypos, combo_layers) \
     ZMK_COMBO_5_ARGS(name, combo_bindings, keypos, combo_layers, COMBO_TERM)
 #define ZMK_COMBO_5_ARGS(name, combo_bindings, keypos, combo_layers, combo_timeout) \
-/ { \
+    / { \
         combos { \
             compatible = "zmk,combos"; \
             combo_ ## name { \
@@ -87,8 +87,8 @@
             }; \
         }; \
     };
-/* ZMK_CONDITIONAL_LAYER */
 
+/* ZMK_CONDITIONAL_LAYER */
 #define ZMK_CONDITIONAL_LAYER(if_layers, then_layer) \
     / { \
         conditional_layers { \
