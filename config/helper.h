ebb0f6fdf927c7bbc3e85376219bc5c574da2a84
inc0nnu <ivo@tulkens.com>
2026-01-11 21:44:34 +0100

    removed spaces in helper.h

--- config/helper.h
+++ config/helper.h
@@ -75,6 +75,7 @@
 #define ZMK_COMBO_4_ARGS(name, combo_bindings, keypos, combo_layers) \
     ZMK_COMBO_5_ARGS(name, combo_bindings, keypos, combo_layers, COMBO_TERM)
 #define ZMK_COMBO_5_ARGS(name, combo_bindings, keypos, combo_layers, combo_timeout) \
+/ { \
         combos { \
             compatible = "zmk,combos"; \
             combo_ ## name { \
@@ -84,8 +85,8 @@
                 layers = <combo_layers>; \
                 COMBO_HOOK \
             }; \
-        };
-
+        }; \
+    };
 /* ZMK_CONDITIONAL_LAYER */
 
 #define ZMK_CONDITIONAL_LAYER(if_layers, then_layer) \
